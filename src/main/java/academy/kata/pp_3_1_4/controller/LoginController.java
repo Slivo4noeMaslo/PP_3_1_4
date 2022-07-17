85a42a317d9192d3a31719ea120a691dff0c1f05
Slivo4noeMaslo <arey8@list.ru>
2022-07-17 20:09:38 +0300

    add buttonOver18

--- src/main/java/academy/kata/pp_3_1_4/controller/LoginController.java
+++ src/main/java/academy/kata/pp_3_1_4/controller/LoginController.java
@@ -1,9 +1,12 @@
 package academy.kata.pp_3_1_4.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class LoginController {
 
     @GetMapping("/admin")
@@ -14,5 +17,11 @@ public class LoginController {
     @GetMapping("/user")
     public  String userPage() {
         return "userPage";
+    }
+
+    @PreAuthorize("authentication.getPrincipal().getAge() >= 18")
+    @GetMapping("/user/onlyAdult")
+    public String getPageOver18() {
+        return "onlyAdultUserPage";
     }
 }
